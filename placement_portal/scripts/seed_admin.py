6243183174270d4719d77142ceb2ae4ba5200e95
Shubham <coderx882@gmail.com>
2026-04-07 08:53:40 +0530

    Initialized SQLite database and ensured schema creation via SQLAlchemy

--- placement_portal/scripts/seed_admin.py
+++ placement_portal/scripts/seed_admin.py
@@ -1,3 +1,9 @@
+'''
+Admin is created programatically for safety and security reasons
+since the Industry Interaction Cell / Placement Cell coordinators are generally a team of few people ,
+their credentials are seeded manually via this script
+'''
+
 from app import create_app
 from app.extensions import db
 from app.models.user import User
